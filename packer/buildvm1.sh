d6ee9854d39ef069b494e0139d0ed1a6ccde726d
srakesh28 <srakesh28@yahoo.com>
2019-10-26 03:24:59 -0700

    fix cp with sudo

--- packer/buildvm1.sh
+++ packer/buildvm1.sh
@@ -3,7 +3,7 @@
 echo "************* set environment vars"
 wget https://releases.hashicorp.com/packer/1.4.4/packer_1.4.4_linux_amd64.zip
 unzip packer*
-cp -p packer /usr/local/bin/packer
+sudo cp -p packer /usr/local/bin/packer
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 export ARM_CLIENT_ID=$1
 export ARM_CLIENT_SECRET=$2
