b7b01e7ec6717141a3001d473c57382bafa0f128
srakesh28 <srakesh28@yahoo.com>
2019-10-26 12:39:02 -0700

    fixed variables

--- packer/buildvm1.sh
+++ packer/buildvm1.sh
@@ -10,38 +10,27 @@ export ARM_CLIENT_SECRET=$2
 export ARM_SUBSCRIPTION_ID=$3
 export ARM_TENANT_ID=$4
 export ARM_RESOURCE_GROUP_DISKS=$5
+# export SIG variable
 export sig_rg=$7
-echo $7
 export sig_loc=$8
-echo $8
-#export sigrg="rsazsigrg"
+export siggallery_name=$9
+export siggallery_image=$10
+export sig_def=$11
+export sigp_lublisher=$12
+export sig_offer=$13
+export sig_sku=$14
+export os_type=$15
 
-#echo $7
-#export sigloc="westus2"
-#export sigloc=$8
-#echo $8
-#export siggalleryname="rsazsiggallery"
-export siggalleryname=$9
-echo $9
-#export siggalleryimage="rsazcentosImageDefinition"
-export siggalleryimage=$10
 export sigimagever="1.0.7"
 export sigtarget1="EastUS2"
 export sigtarget2="WestUS2"
 export sigreplica="2"
-#export sigplublisher="rsazpublisher"
-export sigplublisher=$11
-#export sigoffer="rszrhel7x"
-export sigoffer=$12
-#export sigsku="7.6"
-export sigsku=$13
-#export ostype="Linux"
-export ostype=$14
+
 
 rm packer-build-output.log
 echo "************* execute packer build drop path $6"
 ## execute packer build and send out to packer-build-output file
-#packer build  -var playbook_drop_path=$6 ./app1.json 2>&1 | tee packer-build-output.log
+packer build  -var playbook_drop_path=$6 ./app1.json 2>&1 | tee packer-build-output.log
 
 ## export output variable to VSTS 
 export manageddiskname=$(cat packer-build-output.log | grep ManagedImageName: | awk '{print $2}')
@@ -49,7 +38,6 @@ echo "variable $manageddiskname"
 echo "##vso[task.setvariable variable=manageddiskname]$manageddiskname"
 
 export managedimageid=$(cat packer-build-output.log | grep ManagedImageId: | awk '{print $2}')
-#export managedimageid="/subscriptions/8905234f-18fc-4cd8-b132-04d22f4ecb2e/resourceGroups/vsts-terraform-ansible-packer-rg/providers/Microsoft.Compute/images/demoPackerImage-2019-10-26_10_28_11"
 echo "variable $managedimageid"
 echo "##vso[task.setvariable variable=managedimageid]$managedimageid"
 
@@ -62,15 +50,27 @@ echo "##vso[task.setvariable variable=managedimagelocation]$managedimagelocation
 # [ -z "$managedimagelocation" ] && exit 1 || exit 0
 
 #AZ SIG commands
+
 az login --service-principal -u $1 -p $2 --tenant $4
 az account set --subscription $3
+
 #az group create --name $sigrg --location $sigloc
+
 #az sig create --resource-group $sigrg --gallery-name $siggalleryname
+
 # Create Image definition
+
 #az sig image-definition create --resource-group $sigrg --gallery-name $siggalleryname --gallery-image-definition $siggalleryimage --publisher $sigplublisher --offer $sigoffer --sku $sigsku --os-type $ostype
+
 echo "# Create Image version"
+
 #az sig image-version create -g $sigrg --gallery-name $siggalleryname --gallery-image-definition $siggalleryimage --gallery-image-version $sigimagever --managed-image $managedimageid
+
+az sig image-version create -g $7 --gallery-name $9 --gallery-image-definition $11 --gallery-image-version $sigimagever --managed-image $managedimageid
+
 echo "# Add Image to Target regions"
 #az sig image-version create --resource-group $sigrg --gallery-name $siggalleryname --gallery-image-definition $siggalleryimage --gallery-image-version $sigimagever --managed-image $managedimageid --target-regions "$sigtarget1" "$sigtarget2"
+
+az sig image-version create --resource-group $7 --gallery-name $9 --gallery-image-definition $11 --gallery-image-version $sigimagever --managed-image $managedimageid --target-regions "$sigtarget1" "$sigtarget2"
 
 [ -z "$manageddiskname" ] && exit 1 || exit 0
