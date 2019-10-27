812f47a2208e2a4a3426221eac8560b976e5963d
srakesh28 <srakesh28@yahoo.com>
2019-10-27 02:59:43 -0700

    removed rg tag

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -12,9 +12,7 @@ resource "azurerm_resource_group" "demo_resource_group" {
   name     = "packerdemocreate"
   location = "westus2"
 
-  tags {
-    environment = "Packer Demo RG"
-  }
+  
 }
 
 # Create virtual network
