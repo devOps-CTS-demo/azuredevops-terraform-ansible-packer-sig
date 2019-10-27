81f15a5372cdac8b3d0a08e0999aa016f3364ecf
srakesh28 <srakesh28@yahoo.com>
2019-10-27 12:04:00 -0700

    added rg tag with ==

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -12,6 +12,9 @@ resource "azurerm_resource_group" "demo_resource_group" {
   name     = "packerdemocreate"
   location = "westus2"
 
+   tags = {
+    environment = "Packer Demo Build"
+  }
   
 }
 
